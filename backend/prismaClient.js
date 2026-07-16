14ee87ca5d88fb5935eba4c4ca87d4ceaf17e893
avinashshanbhag77 <avinashshanbhag77@gmail.com>
2026-07-17 00:02:09 +0530

    FIX: Remove unsupported datasourceUrl from constructor and add url to schema.prisma

--- backend/prismaClient.js
+++ backend/prismaClient.js
@@ -1,10 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import dotenv from 'dotenv'
 
-dotenv.config()
-
-const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-})
+const prisma = new PrismaClient()
 
 export default prisma
